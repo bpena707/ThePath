a7806c2facb85d1e61b10bd774b28da1fc07f215
bpena707 <46693930+bpena707@users.noreply.github.com>
2021-11-20 00:21:53 -0800

    Add files via upload

---
+++ src/Version1/ISteerable.java
@@ -0,0 +1,20 @@
+package com.mycompany.a1;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface ISteerable is going to make the ant turn left and right
+ * it will be implemented in ant class
+ */
+public interface ISteerable {
+	
+	/**
+	 * Turn left.
+	 */
+	public void turnLeft();
+
+    /**
+     * Turn right.
+     */
+    public void turnRight();
+
+}
