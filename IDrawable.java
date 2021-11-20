2102815b325edd6fec9897c03a57361cb00bb7a3
bpena707 <46693930+bpena707@users.noreply.github.com>
2021-11-20 00:14:01 -0800

    Delete IDrawable.java

--- IDrawable.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.a3;
-
-import com.codename1.charts.models.Point;
-import com.codename1.ui.Graphics;
-
-public interface IDrawable {
-	public void draw(Graphics g, Point pCmpRelPrnt);
-
-}
