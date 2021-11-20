daf085ab8f30dbcb37e89278ff4c1cdb386a40cc
bpena707 <46693930+bpena707@users.noreply.github.com>
2021-11-20 00:14:11 -0800

    Delete IIterator.java

--- IIterator.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.a3;
-
-public interface IIterator {
-	//iterator provides access at implementation to iterate through the array
-	public boolean hasNext();
-	public GameObject getNext();
-
-}
-
