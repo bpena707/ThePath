ddbbf88a89e4249326e7423cccac9d72e3c3af54
bpena707 <46693930+bpena707@users.noreply.github.com>
2021-11-20 00:13:49 -0800

    Delete ICollider.java

--- ICollider.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.a3;
-
-public interface ICollider {
-	
-	public boolean collidesWith(GameObject otherObject);
-	public void handleCollision(GameObject otherObject, GameWorld gw);
-
-}
